3c94cf2931adc8e1971a19f7958cc88c2ecd09e2
Anamika <avikajha@Avikas-iMac.local>
2021-11-25 14:56:04 -0800

    Added resolver and completed server side of work

--- server/server.js
+++ server/server.js
@@ -31,8 +31,6 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
